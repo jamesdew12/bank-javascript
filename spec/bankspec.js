7aa67ee0e8060d96c53e6ca1ec545e8a24ccc174
James de Winton <james.dewinton@gmail.com>
2019-03-18 15:44:51 +0000

    can deposit money

--- spec/bankspec.js
+++ spec/bankspec.js
@@ -7,7 +7,10 @@ describe('Bank;', function(){
   });
   it('has an intial balance of zero', function(){
     expect(bank.balance()).toEqual(0);
-
+  });
+  it('can add 10 to bank account', function(){
+    bank.deposit(10);
+    expect(bank.balance()).toEqual(10);
   });
 
 
--- src/bank.js
+++ src/bank.js
@@ -6,3 +6,7 @@ function Bank(){
 
 Bank.prototype.balance = function () {
 return this._current_balance; };
+
+Bank.prototype.deposit = function (amount) {
+  this._current_balance += 10;
+  return this._current_balance; };
